243e54dd52e71e5846e8cd4879e5891d79b9864b
Josh <a0526741852@gmail.com>
2025-05-15 20:26:18 +0300

    Seperated_migration_from_docker

---
+++ backend/code for migration.js
@@ -0,0 +1,10 @@
+/*
+
+docker-compose exec backend npm run build
+docker-compose exec backend npm run migrate:run
+docker-compose exec backend npm run build
+docker-compose exec backend npm run seed
+docker-compose exec backend npm run seed:advanced
+
+
+*/
